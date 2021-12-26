5bbf3267e98a37c173bcf86d67d728b1ce900ab9
NeowZhiChin <zhichin99@gmail.com>
2021-12-26 12:57:05 +0800

    update

--- src/hooks/patch-person.js
+++ src/hooks/patch-person.js
@@ -35,7 +35,38 @@ module.exports = (options = {}) => {
       }
       
     }
+    
+    if(data.password)
+    {
+      pw_validation(data.password);
+    }
+  
 
     return context;
   };
 };
+
+function pw_validation(pw)
+{
+  if (pw.length < 12 || pw.length > 16) 
+  {
+    throw new Error("Your password must between 12 to 16 characters");
+  }
+  if (pw.search(/[a-z]/i) < 0) 
+  {
+    throw new Error("Your password must contain at least one letter");
+  }
+  if (pw.search(/[0-9]/) < 0) 
+  {
+    throw new Error("Your password must contain at least one digit."); 
+  }
+  if (pw.search(/[A-Z]/) < 0 )
+  {
+    throw new Error("Your password must contain at least one capital letter.");
+  }
+  if (pw.search(/[^A-Za-z0-9]/)< 0 )
+  {
+    throw new Error("Your password must contain at least one special character");
+  }
+
+}
